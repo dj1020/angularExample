867624b7e1c744c1b29a4a6400516a2c4ca15361
TwkDJ <dj1020@gmail.com>
2014-11-29 14:51:16 +0800

    "date" Filter Example.

--- app/event/controllers/eventListCtrl.js
+++ app/event/controllers/eventListCtrl.js
@@ -6,23 +6,23 @@ angular.module('eventApp', [])
         $scope.limit = 3;
         $scope.events = [
             {
-                'date': '2014/11/29',
+                'date': Date.parse('2014/11/29'),
                 'name': 'AngularJS 菁英班 - 初階班',
                 'price': 20000.123
             },{
-                'date': '2014/11/30',
+                'date': Date.parse('2014/11/30'),
                 'name': 'AngularJS 菁英班 - 中階班',
                 'price': 35000.456
             },{
-                'date': '2014/12/01',
+                'date': Date.parse('2014/12/01'),
                 'name': 'AngularJS 菁英班 - 高階班',
                 'price': 50000.789
             },{
-                'date': '2014/12/05',
+                'date': Date.parse('2014/12/05'),
                 'name': 'Backbone.js 菁英班 - 上',
                 'price': 15000
             },{
-                'date': '2014/12/06',
+                'date': Date.parse('2014/12/06'),
                 'name': 'Backbone.js 菁英班 - 下',
                 'price': 15000
             }
