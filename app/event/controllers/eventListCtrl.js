cb747366c5c7ae7b3c4359f6e069ade7217462e1
TwkDJ <dj1020@gmail.com>
2014-11-29 15:23:40 +0800

    Multiple query filter example. (但怪怪的，老師說是不會自動 "and")

--- app/event/controllers/eventListCtrl.js
+++ app/event/controllers/eventListCtrl.js
@@ -4,25 +4,25 @@ angular.module('eventApp', [])
         // Lession 5-1
 
         $scope.limit = 3;
-        $scope.events = [
+        $scope.items = [
             {
-                'date': Date.parse('2014/11/29'),
+                'date': '2014/11/29',
                 'name': 'AngularJS 菁英班 - 初階班',
-                'price': 20000.123
+                'price': 15000
             },{
-                'date': Date.parse('2014/11/30'),
+                'date': '2014/11/30',
                 'name': 'AngularJS 菁英班 - 中階班',
                 'price': 35000.456
             },{
-                'date': Date.parse('2014/12/01'),
+                'date': '2014/12/01',
                 'name': 'AngularJS 菁英班 - 高階班',
                 'price': 50000.789
             },{
-                'date': Date.parse('2014/12/05'),
+                'date': '2014/12/05',
                 'name': 'Backbone.js 菁英班 - 上',
                 'price': 15000
             },{
-                'date': Date.parse('2014/12/06'),
+                'date': '2014/12/06',
                 'name': 'Backbone.js 菁英班 - 下',
                 'price': 15000
             }
