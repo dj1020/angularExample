099bf603cb69d2a41aa3f605362687633cde72b4
TwkDJ <dj1020@gmail.com>
2014-11-29 13:57:21 +0800

    List item from Model

--- app/event/controllers/eventListCtrl.js
+++ app/event/controllers/eventListCtrl.js
@@ -3,19 +3,21 @@ angular.module('eventApp', [])
         // Lession 5
         // Lession 5-1
 
+        $scope.events = [
+            {
+                'date': '2014/11/29',
+                'name': 'AngularJS 菁英班 - 初階班'
+            },{
+                'date': '2014/11/30',
+                'name': 'AngularJS 菁英班 - 中階班'
+            },{
+                'date': '2014/12/01',
+                'name': 'AngularJS 菁英班 - 高階班'
+            }
+        ];
+
         $scope.Test = function (){
-            $scope.events = [
-                {
-                    'date': '2014/11/29',
-                    'name': 'AngularJS 菁英班 - 初階班'
-                },{
-                    'date': '2014/11/30',
-                    'name': 'AngularJS 菁英班 - 中階班'
-                },{
-                    'date': '2014/12/01',
-                    'name': 'AngularJS 菁英班 - 高階班'
-                }
-            ];
+            alert('test');
         };
 
     });
