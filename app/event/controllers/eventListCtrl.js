d2ab00ad5c16a8ff5205f8eea488d7caf288976c
TwkDJ <dj1020@gmail.com>
2014-11-29 14:43:03 +0800

    "number" Filter Example.

--- app/event/controllers/eventListCtrl.js
+++ app/event/controllers/eventListCtrl.js
@@ -6,13 +6,24 @@ angular.module('eventApp', [])
         $scope.events = [
             {
                 'date': '2014/11/29',
-                'name': 'AngularJS 菁英班 - 初階班'
+                'name': 'AngularJS 菁英班 - 初階班',
+                'price': 20000.123
             },{
                 'date': '2014/11/30',
-                'name': 'AngularJS 菁英班 - 中階班'
+                'name': 'AngularJS 菁英班 - 中階班',
+                'price': 35000.456
             },{
                 'date': '2014/12/01',
-                'name': 'AngularJS 菁英班 - 高階班'
+                'name': 'AngularJS 菁英班 - 高階班',
+                'price': 50000.789
+            },{
+                'date': '2014/12/05',
+                'name': 'Backbone.js 菁英班 - 上',
+                'price': 15000
+            },{
+                'date': '2014/12/06',
+                'name': 'Backbone.js 菁英班 - 下',
+                'price': 15000
             }
         ];
 
