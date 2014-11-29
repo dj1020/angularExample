b58c7c5ecaddb0cd58dd414c708d3cd7c4fe9b2c
TwkDJ <dj1020@gmail.com>
2014-11-29 13:56:23 +0800

    Angular JS controller, binding example

---
+++ app/event/controllers/eventListCtrl.js
@@ -0,0 +1,21 @@
+angular.module('eventApp', [])
+    .controller('eventListCtrl', function ($scope) {
+        // Lession 5
+        // Lession 5-1
+
+        $scope.Test = function (){
+            $scope.events = [
+                {
+                    'date': '2014/11/29',
+                    'name': 'AngularJS 菁英班 - 初階班'
+                },{
+                    'date': '2014/11/30',
+                    'name': 'AngularJS 菁英班 - 中階班'
+                },{
+                    'date': '2014/12/01',
+                    'name': 'AngularJS 菁英班 - 高階班'
+                }
+            ];
+        };
+
+    });
