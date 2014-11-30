f2fa2b55faf044f796b8f3e5e1ee303e8341b290
TwkDJ <dj1020@gmail.com>
2014-11-30 14:23:43 +0800

    ng-options 應用 before ng-route

--- app/event/controllers/eventListCtrl3.js
+++ app/event/controllers/eventListCtrl3.js
@@ -1,11 +1,22 @@
 angular.module("eventApp", [])
-    .controller('eventListCtrl3', function ($scope, $http) {
-
-        $scope.test = "my hello";
+    .controller('eventListCtrl', function ($scope, $http) {
 
         $http.get('app/event/data/event-data.json').success(function (data) {
             $scope.events = data;
-            // console.log(data);
         });
+
+        $scope.reverses = [
+            { 'name': '最新在前', 'shade': false },
+            { 'name': '最新在後', 'shade': true }
+        ];
+
+        $scope.sortFields = [
+            {name : "Date", value: "date"},
+            {name : "Name", value: "name"},
+            {name : "Price", value: "price"}
+        ];
+
+        $scope.reverseVale = $scope.reverses[0];
+        $scope.predicate   = $scope.sortFields[0];
 
     });
