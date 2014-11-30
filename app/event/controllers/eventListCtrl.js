1085c649d8c40029c0e6f2f9c035f2b4154e112d
TwkDJ <dj1020@gmail.com>
2014-11-30 10:11:57 +0800

    發現 json 檔案中要用 雙引號 double quote

--- app/event/controllers/eventListCtrl.js
+++ app/event/controllers/eventListCtrl.js
@@ -6,25 +6,25 @@ angular.module('eventApp', ['eventFilters'])
         $scope.limit = 3;
         $scope.items = [
             {
-                'date': '2014/11/29',
-                'name': 'AngularJS 菁英班 - 初階班',
-                'price': 15000
+                "date": "2014/11/29",
+                "name": "AngularJS 菁英班 - 初階班",
+                "price": 15000
             },{
-                'date': '2014/11/30',
-                'name': 'AngularJS 菁英班 - 中階班',
-                'price': 35000.456
+                "date": "2014/11/30",
+                "name": "AngularJS 菁英班 - 中階班",
+                "price": 35000.456
             },{
-                'date': '2014/12/01',
-                'name': 'AngularJS 菁英班 - 高階班',
-                'price': 50000.789
+                "date": "2014/12/01",
+                "name": "AngularJS 菁英班 - 高階班",
+                "price": 50000.789
             },{
-                'date': '2014/12/05',
-                'name': 'Backbone.js 菁英班 - 上',
-                'price': 15000
+                "date": "2014/12/05",
+                "name": "Backbone.js 菁英班 - 上",
+                "price": 15000
             },{
-                'date': '2014/12/06',
-                'name': 'Backbone.js 菁英班 - 下',
-                'price': 15000
+                "date": "2014/12/06",
+                "name": "Backbone.js 菁英班 - 下",
+                "price": 15000
             }
         ];
 
